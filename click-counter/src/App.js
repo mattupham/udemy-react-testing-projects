fb28557d36dbdb9ab4e4fe716e5dab3c71911e55
Matt Upham <matt.upham@afterpay.com>
2018-12-31 17:54:29 -0800

    Set up initial files for testing

---
+++ click-counter/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment Button</button>
+      </div>
+    );
+  }
+}
+
+export default App;
