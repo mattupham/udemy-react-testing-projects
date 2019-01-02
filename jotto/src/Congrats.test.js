527eb9c8c3acefb59eb1ae13ab85cfff80f43124
Matt Upham <matt.upham@afterpay.com>
2019-01-01 22:34:01 -0800

    Change props setup

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,10 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
+import checkPropTypes from "check-prop-types";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { success: false };
 
 /**
  * Factory function to create a ShallWrapper for the App component
@@ -15,11 +19,12 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  */
 
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
@@ -34,4 +39,9 @@ test("renders non-empty congrats message when success prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
