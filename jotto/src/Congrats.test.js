499bed1aa7b66b5402a7d90c2560f40f5261f1c5
Matt Upham <matt.upham@afterpay.com>
2019-01-01 22:36:52 -0800

    Move out enzyme configure

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,12 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import checkPropTypes from "check-prop-types";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 
