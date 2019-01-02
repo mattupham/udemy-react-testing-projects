27b5029fde12883629cb1fd6d69ccf302a62dfec
Matt Upham <matt.upham@afterpay.com>
2019-01-01 18:55:00 -0800

    Add congrats component and tests

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -7,6 +7,16 @@ import PropTypes from "prop-types";
  * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
-export default () => {
-  return <div />;
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
